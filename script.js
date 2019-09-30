7750637fc16860886b0cfd1730cef09b78c0864d
Marti <52050778+github-marti@users.noreply.github.com>
2019-09-29 20:10:38 -0700

    changed time to unix so that dates adhered to local timezones and adjusted search function so that it will not store searches that do not produce results

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // global variables
 const apiKey = "32d68f82674d25a88e4344fd3ae53c80"
-const today = moment().format('MM/DD/YYYY');
 let citiesArray = [];
 
 // function for getting local weather based on geolocation
 function getLocalWeather(position) {
+    // getting local time
+    const localDay = moment().format('MM/DD/YYYY');
     // code to get data from api
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
@@ -20,19 +21,16 @@ function getLocalWeather(position) {
         // get weather icon url
         let iconURL = `https://openweathermap.org/img/w/${response.weather[0].icon}.png`
 
-        // convert m/s to knots
-        const windSpeed = response.wind.speed * 1.944
-
         // call UV index function
         getUVIndex(lat, lon);
 
         // populates text with data from weather API
         $('#city-name').text(`${response.name}, ${response.sys.country}`);
-        $('#current-date').text(`(${today})`);
+        $('#current-date').text(`(${localDay})`);
         $('#weather-icon').attr('src', iconURL);
         $('#current-temp').text(`${response.main.temp.toFixed(1)} \xB0F`);
         $('#current-hum').text(`${response.main.humidity}%`)
-        $('#wind-speed').text(`${windSpeed.toFixed(2)} knots`)
+        $('#wind-speed').text(`${response.wind.speed.toFixed(2)} mi/s`)
     });
 
     queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
@@ -72,6 +70,9 @@ function getCurrentWeather(city) {
     }).then(function(response) {
         console.log(response);
 
+        // get the local time
+        const unixTime = response.dt + response.timezone;
+        let today = moment.unix(unixTime).format("MM/DD/YYYY");
 
         // get latitude and longitude for the UV index
         const lat = response.coord.lat
@@ -79,9 +80,6 @@ function getCurrentWeather(city) {
 
         // get weather icon url
         let iconURL = `https://openweathermap.org/img/w/${response.weather[0].icon}.png`
-
-        // convert m/s to knots
-        const windSpeed = response.wind.speed * 1.944
 
         // call UV index function
         getUVIndex(lat, lon);
@@ -92,13 +90,19 @@ function getCurrentWeather(city) {
         $('#weather-icon').attr('src', iconURL);
         $('#current-temp').text(`${response.main.temp.toFixed(1)} \xB0F`);
         $('#current-hum').text(`${response.main.humidity}%`)
-        $('#wind-speed').text(`${windSpeed.toFixed(2)} knots`)
-    
+        $('#wind-speed').text(`${response.wind.speed.toFixed(2)} mi/s`)
+        
+        // calls function to get five day forecast
+        getFiveDay(city, unixTime);
+
+        // stores input in local storage
+        storeData(city);
+
     })
 }
 
 // function for getting five day forecast
-function getFiveDay(city) {
+function getFiveDay(city, unixTime) {
     // code to get data from api
     let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
 
@@ -109,10 +113,11 @@ function getFiveDay(city) {
         console.log(response);
         // sets array to start at 12 noon on following day
         let arrayIndex = 4;
+        console.log(unixTime);
     
         // for loop that runs 5 times to populate 5 day forecast cards
         for (i = 1; i < 6; i++){
-            let day = moment().add(i, 'days').format('MM/DD/YYYY');
+            let day = moment.unix(unixTime).add(i, 'days').format('MM/DD/YYYY');
             let iconURL = `https://openweathermap.org/img/w/${response.list[arrayIndex].weather[0].icon}.png`
 
             $(`#forecast-date-${i}`).text(day);
@@ -174,19 +179,20 @@ function loadData() {
     };
 
     getCurrentWeather(storedCities[storedCities.length-1]);
-    getFiveDay(storedCities[storedCities.length-1]);
 }
 
 // function for saving data to local storage
 function storeData(city) {
     // pushes new city query into array and puts array into local storage
-    citiesArray.push(city);
-    localStorage.setItem('cities', JSON.stringify(citiesArray));
+    if (citiesArray[citiesArray.length-1] !== city) {
+        citiesArray.push(city);
+        localStorage.setItem('cities', JSON.stringify(citiesArray));
 
-    // adds a list item to search display
-    let p = $('<p>').text(city);
-    p.addClass('list-item');
-    $('#search-display').prepend(p);
+        // adds a list item to search display
+        let p = $('<p>').text(city);
+        p.addClass('list-item');
+        $('#search-display').prepend(p);
+    }
 }
 
 // function for clearing data from local storage
@@ -204,10 +210,6 @@ $('#submit-button').on('click', function() {
 
     // calls current weather and five day forecast functions
     getCurrentWeather(city);
-    getFiveDay(city);
-
-    // stores input in local storage
-    storeData(city);
 })
 
 // event listener for previous search items
@@ -215,7 +217,6 @@ $('#search-display').on('click', '.list-item', function() {
     // calls weather functions
     let city = $(this).text();
     getCurrentWeather(city)
-    getFiveDay(city);
 })
 
 // event listener for clear button
